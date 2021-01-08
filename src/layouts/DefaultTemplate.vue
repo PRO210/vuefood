585564c2035cf46b9198e5dd007698122b614b8c
Andre Freitas da Silva <proandre21@hotmail.com>
2021-01-08 12:20:52 -0300

    mudança para o windows

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul><li><router-link :to="{name: 'home'}">home</router-link></li></ul>
+        <ul><li><router-link :to="{name: 'loja'}">loja</router-link></li></ul>
+        <ul><li><router-link :to="{name: 'cart'}">cart</router-link></li></ul>
+
+
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default {
+    
+}
+</script>
+
+<style scope>
+    @import url('../assets/css/vuefood.css');
+    @import url('../assets/css/reset.css');
+</style>
